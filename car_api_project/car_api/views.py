fd0e1a93b6668d99318c21ba9558210570863614
rihan <Rihanfoudeh@gmail.com>
2021-12-14 13:23:01 +0200

    first commit

---
+++ car_api_project/car_api/views.py
@@ -0,0 +1,18 @@
+
+# from django.shortcuts import render
+from rest_framework import generics
+from .serializers import CarsSerializer
+from .models import Car
+from .permissions import IsOwnerOrReadOnly
+# Create your views here.
+
+class CarsList(generics.ListCreateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarsSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+
+class CarsDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    queryset = Car.objects.all()
+    serializer_class = CarsSerializer
